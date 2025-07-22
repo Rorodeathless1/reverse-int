1cb4991b643973b98a1d22fe0939be104dadeeff
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-07-22 12:55:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const num = Math.abs(n);
+  const str = num.toString();
+  const arr = str.split('');
+  const reversedArr = arr.reverse();
+  const revetsedStr = reversedArr.join('');
+  const reversedNum = +revetsedStr;
+
+  return reversedNum;
 };
